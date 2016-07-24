3b67e5496dba04f1413588b15b443c4ac59d4a82
Nicholas D. Socci <soccin@mskcc.org>
2016-07-24 16:47:52 -0400

    Added stage 4: bam to hitmap

--- pipe.sh
+++ pipe.sh
@@ -12,6 +12,7 @@ GENOME_DIR=/ifs/res/socci/LUX/ifs/data/bio/Genomes/S.cerevisiae/sacCer2/SGD/2008
 GENOME_FASTA=$GENOME_DIR/SGD_sacCer2.fa
 GENOME_TAG=SGD_sacCer2
 GENOME_INDEX=$GENOME_DIR/SHRiMP/DNA/$GENOME_TAG-ls
+GENOME_BEDTOOLS=$GENOME_DIR/SGD_sacCer2.genome
 
 SAMPLENAME=$1
 shift 1
@@ -37,17 +38,23 @@ for SAMPLEDIR in $SAMPLEDIRS; do
 
         CLIPFASTQ=$ODIR/$BLOCKNUM/$(basename $FASTQ | sed 's/.fastq.gz//')___CLIP.fastq.gz
 
-        bsub -o LSF/ -J ${TAG}_2_$BLOCKNUM -w "post_done(${TAG}_1_$BLOCKNUM)" -n 24 \
+        bsub -o LSF/ -J ${TAG}_2_$BLOCKNUM -w "post_done(${TAG}_1_$BLOCKNUM)" -n 24 -We 59 \
             $SDIR/mapSHRiMP_SE.sh $GENOME_INDEX $CLIPFASTQ $SAMPLENAME
 
         SAM=$(echo $CLIPFASTQ | sed 's/.fastq.gz/___SHR_SE.sam/')
 
-        bsub -o LSF/ -J ${TAG}_3_$BLOCKNUM -w "post_done(${TAG}_2_$BLOCKNUM)" -R "rusage[mem=36]" -n 3 \
-            picard.local SortSam I=$SAM O=${SAM/.sam/.bam} SO=coordinate INDEX=true
+        bsub -o LSF/ -J ${TAG}_3_$BLOCKNUM -w "post_done(${TAG}_2_$BLOCKNUM)" -R "rusage[mem=36]" -n 3 -We 59\
+            picard.local SortSam I=$SAM O=${SAM/.sam/.bam} SO=coordinate CREATE_INDEX=true
+
+        bsub -o LSF/ -J ${TAG}_4_$BLOCKNUM -w "post_done(${TAG}_3_$BLOCKNUM)" -n 3 -We 59 \
+            $SDIR/bam2UniqueStrandHitMap.sh $GENOME_BEDTOOLS ${SAM/.sam/.bam}
 
         BLOCKNUM=$((BLOCKNUM+1))
 
     done
 done
 
-bSync ${TAG}_3_'\d+'
+~/Code/LSF/bSync/bHold ${TAG}_4_'\d+'
+~/bin/notify.sh DONE
+
+#bSync ${TAG}_3_'\d+'
