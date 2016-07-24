0d994bd2fc535e276a42751e8d392f06d939b996
Nicholas D. Socci <soccin@mskcc.org>
2016-07-24 16:16:52 -0400

    Fixed thread count on map stage and added sort

--- pipe.sh
+++ pipe.sh
@@ -29,18 +29,25 @@ TAG=qS1SEQ_$$__$(uuidgen)
 BLOCKNUM=1
 for SAMPLEDIR in $SAMPLEDIRS; do
     for FASTQ in $(ls $SAMPLEDIR/*_R1_*fastq.gz); do
+
         mkdir -p $ODIR/$BLOCKNUM
-        echo $FASTQ
+
         bsub -o LSF/ -J ${TAG}_1_$BLOCKNUM -We 59 \
             $SDIR/clipAdapterSE.sh $ODIR/$BLOCKNUM $FASTQ
 
         CLIPFASTQ=$ODIR/$BLOCKNUM/$(basename $FASTQ | sed 's/.fastq.gz//')___CLIP.fastq.gz
-        bsub -o LSF/ -J ${TAG}_2_$BLOCKNUM -w "post_done(${TAG}_1_$BLOCKNUM)" \
+
+        bsub -o LSF/ -J ${TAG}_2_$BLOCKNUM -w "post_done(${TAG}_1_$BLOCKNUM)" -n 24 \
             $SDIR/mapSHRiMP_SE.sh $GENOME_INDEX $CLIPFASTQ $SAMPLENAME
+
+        SAM=$(echo $CLIPFASTQ | sed 's/.fastq.gz/___SHR_SE.sam/')
+
+        bsub -o LSF/ -J ${TAG}_3_$BLOCKNUM -w "post_done(${TAG}_2_$BLOCKNUM)" -R "rusage[mem=36]" -n 3 \
+            picard.local SortSam I=$SAM O=${SAM/.sam/.bam} SO=coordinate INDEX=true
 
         BLOCKNUM=$((BLOCKNUM+1))
 
     done
 done
 
-bSync ${TAG}_2_'\d+'
+bSync ${TAG}_3_'\d+'
