d20bd0842b704a575239c45f4df717293501b888
Nicholas D. Socci <soccin@mskcc.org>
2016-10-16 14:16:31 -0400

    Added MEMSIZE for mouse mapping removed -We

--- pipe.sh
+++ pipe.sh
@@ -53,22 +53,22 @@ for SAMPLEDIR in $SAMPLEDIRS; do
 
         mkdir -p $ODIR/$BLOCKNUM
 
-        bsub -o LSF/ -J ${TAG}_1_$BLOCKNUM -We 59 \
+        bsub -We 59 -o LSF/ -J ${TAG}_1_$BLOCKNUM \
             $SDIR/clipAdapterSE.sh $ODIR/$BLOCKNUM $FASTQ $SDIR/adapter_TruSeqFull.sh
 
         CLIPFASTQ=$ODIR/$BLOCKNUM/$(basename $FASTQ | sed 's/.fastq.gz//')___CLIP.fastq.gz
 
         bsub -o LSF/ -J ${TAG}_2_$BLOCKNUM -w "post_done(${TAG}_1_$BLOCKNUM)" \
-            -n 24 -We 59 \
+            -n 24 \
             -R "rusage[mem=$MEMSIZE]" \
             $SDIR/mapSHRiMP_SE.sh $GENOME_INDEX $CLIPFASTQ $SAMPLENAME
 
         SAM=$(echo $CLIPFASTQ | sed 's/.fastq.gz/___SHR_SE.sam/')
 
-        bsub -o LSF/ -J ${TAG}_3_$BLOCKNUM -w "post_done(${TAG}_2_$BLOCKNUM)" -R "rusage[mem=36]" -n 3 -We 59\
+        bsub -We 59 -o LSF/ -J ${TAG}_3_$BLOCKNUM -w "post_done(${TAG}_2_$BLOCKNUM)" -R "rusage[mem=36]" -n 3 \
             picard.local SortSam I=$SAM O=${SAM/.sam/.bam} SO=coordinate CREATE_INDEX=true
 
-        bsub -o LSF/ -J ${TAG}_4_$BLOCKNUM -w "post_done(${TAG}_3_$BLOCKNUM)" -n 3 -We 59 \
+        bsub -We 59 -o LSF/ -J ${TAG}_4_$BLOCKNUM -w "post_done(${TAG}_3_$BLOCKNUM)" -n 3 \
             $SDIR/bam2UniqueStrandHitMap.sh $GENOME_BEDTOOLS ${SAM/.sam/.bam}
 
         HITMAPS[$BLOCKNUM]=${SAM/.sam/}
@@ -80,18 +80,19 @@ done
 
 bSync ${TAG}_4_'\d+'
 
-bsub -o LSF/ -J ${TAG}_5 -n 3 -R "rusage[mem=36]" \
+bsub -We 59 -o LSF/ -J ${TAG}_5 -n 3 -R "rusage[mem=36]" \
     picard.local MergeSamFiles O=$ODIR/${SAMPLENAME}___merge.bam CREATE_INDEX=true \
     $(find $ODIR | fgrep .bam | fgrep -v merge.bam | awk '{print "I="$1}')
 
-bsub -o LSF/ -J ${TAG}_6 -w "post_done(${TAG}_5)" -n 3 \
-    $SDIR/getUniqueMaps.sh $ODIR/${SAMPLENAME}___merge.bam $ODIR/${SAMPLENAME}___merge,unique.bam
+bsub -We 59 -o LSF/ -J ${TAG}_6 -w "post_done(${TAG}_5)" -n 3 \
+    $SDIR/getUniqueMaps.sh $ODIR/${SAMPLENAME}___merge.bam \
+    $ODIR/${SAMPLENAME}___merge,unique.bam
 
-bsub -o LSF/ -J ${TAG}_7 -w "post_done(${TAG}_5)" -n 3 -R "rusage[mem=36]" \
+bsub -We 59 -o LSF/ -J ${TAG}_7 -w "post_done(${TAG}_5)" -n 3 -R "rusage[mem=36]" \
     picard.local CollectAlignmentSummaryMetrics R=$GENOME_FASTA \
     O=$ODIR/${SAMPLENAME}___merge___AS.txt I=$ODIR/${SAMPLENAME}___merge.bam
 
-bsub -o LSF/ -J ${TAG}_8 -n 3 -We 59 \
+bsub -We 59 -o LSF/ -J ${TAG}_8 -n 3 \
     $RSCRIPT --no-save $SDIR/mergeHitMaps.R \
         $ODIR/${SAMPLENAME}_HITMAP_.Rdata \
         $SAMPLENAME \
