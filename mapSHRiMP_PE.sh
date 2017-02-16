950b7e79652420e89af97d0801cb1e7221abb9e9
Nicholas D. Socci <soccin@mskcc.org>
2017-02-16 14:48:59 -0500

    PE Mapping for SHRiMP

--- mapSHRiMP_PE.sh
+++ mapSHRiMP_PE.sh
@@ -7,11 +7,13 @@ FASTQ=$2
 SAMPLENAME=$3
 ODIR=$(dirname $FASTQ)
 
-OUTSAM=$(echo $FASTQ | sed 's/.fastq.gz/___SHR_SE.sam/')
+OUTSAM=$(echo $FASTQ | sed 's/.fastq/___SHR_PE.sam/')
 $SDIR/bin/SHRiMP_2_1_1b/bin/gmapper-ls \
     -E -U -n 1 -Q --sam-unaligned --strata \
     -o 11 -N 24 \
+    -p opp-in -I 50,500 \
     --read-group ${SAMPLENAME},${SAMPLENAME} --sam-unaligned \
     -L $GENOME_INDEX \
-    $FASTQ \
+    -1 $FASTQ \
+    -2 ${FASTQ/_R1_/_R2_} \
     >$OUTSAM
