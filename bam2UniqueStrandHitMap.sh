dd493224ca7e282f829eac7a011c5fb3b0dfd8e0
Nicholas D. Socci <soccin@mskcc.org>
2017-02-16 14:49:42 -0500

    change bam2HM to work with PE maps

--- bam2UniqueStrandHitMap.sh
+++ bam2UniqueStrandHitMap.sh
@@ -5,19 +5,33 @@ BASE=${BAM%%.bam}
 echo $BASE, $BAM
 
 BEDTOOLS=/opt/common/CentOS_6-dev/bedtools/bedtools-2.25.0/bin/bedtools
+SAMTOOLS=/opt/common/CentOS_6-dev/samtools/samtools-1.3.1/samtools
+
+
+#
+# First get unique properly paired reads
+#
+
+(
+    $SAMTOOLS view -H $BAM;
+    $SAMTOOLS view -f2 $BAM| fgrep -w "NH:i:1"
+    ) \
+| samtools view -Sb - >${BASE}___UNIQ_PP.bam
 
 #
 # NH tag in SHRiMP is number of hits
 # -tag NH puts NH number in col 5
 # $5==1 UNIQUE hits
 
-$BEDTOOLS bamtobed -tag NH -i $BAM \
-    | awk '$5==1 && $6=="+"{print $1,$2,$2+1,$0}' \
+$BEDTOOLS bamtobed -bedpe -i ${BASE}___UNIQ_PP.bam \
+    | awk '$9=="+"{print $1,$2,$2+1,$0}' \
     | tr ' ' '\t' \
+    | sort -k1,1V -k2,2n \
     | $BEDTOOLS genomecov -i - -g $GENOME -d >${BASE}__PosHM.txt
 
-$BEDTOOLS bamtobed -tag NH -i $BAM \
-    | awk '$5==1 && $6=="-"{print $1,$3-1,$3,$0}' \
+$BEDTOOLS bamtobed -bedpe -i ${BASE}___UNIQ_PP.bam \
+    | awk '$9=="-"{print $1,$6-1,$6,$0}' \
     | tr ' ' '\t' \
+    | sort -k1,1V -k2,2n \
     | $BEDTOOLS genomecov -i - -g $GENOME -d >${BASE}__NegHM.txt
 
