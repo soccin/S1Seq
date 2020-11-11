209a643be620af78b142ba228f9f06debcc0c3c5
Nicholas D. Socci <soccin@mskcc.org>
2020-11-11 16:52:54 -0500

    Last LUNA Version

--- splitFastq.sh
+++ splitFastq.sh
@@ -2,7 +2,7 @@
 
 #
 # Usage:
-#    bsub -n 2 -We 59 -o LSF/ -J SPLIT ./splitFastq.sh file.fastq.gz
+#    bsub -n 2 -W 59 -o LSF/ -J SPLIT ./splitFastq.sh file.fastq.gz
 #
 
 file=$1
@@ -21,5 +21,5 @@ zcat $file | split -a 3 -d -l 4000000 - $ODIR/${samp/Sample_/}_${read}_
 for file in $(find $ODIR -name "*_"$read"_*" | fgrep -v .fastq); do
     echo $file;
     mv $file ${file}.fastq
-    bsub -We 59 -o LSF/ -J GZIP_$project "gzip ${file}.fastq"
+    bsub -W 59 -o LSF/ -J GZIP_$project "gzip ${file}.fastq"
 done
